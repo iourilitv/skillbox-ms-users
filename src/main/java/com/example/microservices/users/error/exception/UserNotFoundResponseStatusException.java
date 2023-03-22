7253833caf337bb652c0d44ed4acf7a34473239d
iourilitv <iourilitv1965@gmail.com>
2023-03-22 10:33:18 +0300

    3-3_http_codes_errors. Business Error: refactor: - renamed USER_RESOURCE_NAME to RESOURCE_NAME in UserNotFoundResponseStatusException.

--- src/main/java/com/example/microservices/users/error/exception/UserNotFoundResponseStatusException.java
+++ src/main/java/com/example/microservices/users/error/exception/UserNotFoundResponseStatusException.java
@@ -2,14 +2,14 @@ package com.example.microservices.users.error.exception;
 
 public class UserNotFoundResponseStatusException extends ResourceNotFoundResponseStatusException {
 
-    private static final String USER_RESOURCE_NAME = "User";
+    private static final String RESOURCE_NAME = "User";
     private static final String ID_FIELD_NAME = "id";
 
     public UserNotFoundResponseStatusException(long id) {
-        super(USER_RESOURCE_NAME, ID_FIELD_NAME, String.valueOf(id));
+        super(RESOURCE_NAME, ID_FIELD_NAME, String.valueOf(id));
     }
 
     public UserNotFoundResponseStatusException(String fieldName, String fieldValue) {
-        super(USER_RESOURCE_NAME, fieldName, fieldValue);
+        super(RESOURCE_NAME, fieldName, fieldValue);
     }
 }
