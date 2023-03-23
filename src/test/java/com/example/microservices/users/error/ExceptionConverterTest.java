89974d8731072f3bfafd869fff7973069cfd7865
iourilitv <iourilitv1965@gmail.com>
2023-03-23 09:36:16 +0300

    3-3_http_codes_errors. Unit Tests: ExceptionConverterTest: - completed test2_givenUnknownResponseStatusException_thenCorrect_mapException method. The test passes correctly.

--- src/test/java/com/example/microservices/users/error/ExceptionConverterTest.java
+++ src/test/java/com/example/microservices/users/error/ExceptionConverterTest.java
@@ -11,8 +11,10 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import static com.example.microservices.users.error.entity.ErrorId.BUSINESS_ERROR;
+import static com.example.microservices.users.error.entity.ErrorId.UNKNOWN_ERROR;
 import static com.example.microservices.users.util.ErrorTestUtils.buildError;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -41,6 +43,25 @@ public class ExceptionConverterTest {
         try {
             throw new PreconditionFailedResponseStatusException(reason);
         } catch (BaseResponseStatusException ex) {
+            when(errorBuilder.buildError(errorId, httpStatus, ex)).thenReturn(error);
+            var actual = exceptionConverter.mapException(ex);
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    void test2_givenUnknownResponseStatusException_thenCorrect_mapException() {
+        String reason = "Unknown ResponseStatusException";
+        String jExceptionMsg = String.format(
+                "com.example.microservices.users.error.exception.PreconditionFailedResponseStatusException{HttpStatus: 400 BAD_REQUEST, Reason: %s}",
+                reason);
+        HttpStatus httpStatus = INTERNAL_SERVER_ERROR;
+        ErrorId errorId = UNKNOWN_ERROR;
+        Error error = buildError(errorId, httpStatus, jExceptionMsg);
+        ErrorList expected = new ErrorList(error);
+        try {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, reason);
+        } catch (ResponseStatusException ex) {
             when(errorBuilder.buildError(errorId, httpStatus, ex)).thenReturn(error);
             var actual = exceptionConverter.mapException(ex);
             assertEquals(expected, actual);
